2f06017ae8130a0e2afe8d85f34c437904781080
Jeremy M <Jinoshi@IT-020537.local>
2022-11-02 11:39:05 -0500

    11/2/2020

---
+++ script.js
@@ -0,0 +1,11 @@
+let countElement = document.getElementById("count-element")
+let count = 0
+
+function increment() {
+    count = count + 1
+    countElement.innerText = count
+}
+
+function save() {
+    console.log(count)
+}
